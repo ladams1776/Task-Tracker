3d9a5d957f57afb0e55f260e24ed50225d6073f2
larry adams <ladams1776@yahoo.com>
2019-11-09 19:51:32 -0500

    Add CSS styling within the component

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,54 @@
 import Link from 'next/link';
 import Layout from '../components/Layout';
-import fetch from 'isomorphic-unfetch';
 
+function getPosts() {
+  return [
+    { id: 'hello-nextjs', title: 'hello nextjs' },
+    { id: '2-nextjs', title: '2 nextjs' },
+    { id: '3-nextjs', title: '3 nextjs' }
+  ];
+}
 const Index = props => {
   return (
     <Layout>
-      <h1>Batman TV Shows</h1>
+      <h1>My blog</h1>
       <ul>
-        {props.shows.map(show => (
-          <li key={show.id}>
-            <Link href="/p/[id]" as={`/p/${show.id}`}>
-              <a>{show.name}</a>
+        {getPosts().map(post => (
+          <li key={post.id}>
+            <Link href="/p/[id]" as={`/p/${post.id}`}>
+              <a>{post.title}</a>
             </Link>
           </li>
         ))}
       </ul>
+      <style jsx>
+        {`
+          h1,
+          a {
+            font-family: 'Arial';
+          }
+
+          ul {
+            padding: 0;
+          }
+
+          li {
+            list-style: none;
+            margin: 5px 0;
+          }
+
+          a {
+            text-decoration: none;
+            color: blue;
+          }
+
+          a:hover {
+            opacity: 0.6;
+          }
+        `}
+      </style>
     </Layout>
   );
-};
-
-Index.getInitialProps = async function() {
-  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
-  const data = await res.json();
-
-  console.log(`show data fetched. Count: ${data.length}`);
-
-  return {
-    shows: data.map(entry => entry.show)
-  };
 };
 
 export default Index;
