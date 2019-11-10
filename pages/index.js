57fa737755f3b990b4846790fab1c2e57feb065f
larry adams <ladams1776@yahoo.com>
2019-11-10 09:00:06 -0500

    Styling individual components

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import Layout from '../components/Layout';
+import Post from './p/[id]';
 
 function getPosts() {
   return [
@@ -8,17 +9,39 @@ function getPosts() {
     { id: '3-nextjs', title: '3 nextjs' }
   ];
 }
-const Index = props => {
+
+const PostLink = ({ post }) => (
+  <li>
+    <Link href="/p/[id]" as={`/p/${post.id}`}>
+      <a>{post.title}</a>
+    </Link>
+    <style jsx>
+      {`
+        li {
+          list-style: none;
+          margin: 5px 0;
+        }
+
+        a {
+          text-decoration: none;
+          color: blue;
+        }
+
+        a:hover {
+          opacity: 0.6;
+        }
+      `}
+    </style>
+  </li>
+);
+
+export default function Blog() {
   return (
     <Layout>
       <h1>My blog</h1>
       <ul>
         {getPosts().map(post => (
-          <li key={post.id}>
-            <Link href="/p/[id]" as={`/p/${post.id}`}>
-              <a>{post.title}</a>
-            </Link>
-          </li>
+          <PostLink key={post.id} post={post} />
         ))}
       </ul>
       <style jsx>
@@ -31,24 +54,8 @@ const Index = props => {
           ul {
             padding: 0;
           }
-
-          li {
-            list-style: none;
-            margin: 5px 0;
-          }
-
-          a {
-            text-decoration: none;
-            color: blue;
-          }
-
-          a:hover {
-            opacity: 0.6;
-          }
         `}
       </style>
     </Layout>
   );
-};
-
-export default Index;
+}
